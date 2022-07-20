181e8225a688acfb6c23bb52f1cb05d6a8e6a703
uwabaking <s256128@wakayama-u.ac.jp>
2022-07-20 14:28:16 +0900

    シェイクミッションの実装(制限時間とシェイクのみ)

---
+++ Assets/Scripts/Mission Scripts/Shake.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+//using TMPro;
+
+public class Shake : MonoBehaviour
+{
+
+    private Vector3 Acceleration;
+    private Vector3 preAcceleration;
+    float DotProduct;
+    public static int ShakeCount;
+    //public TextMeshProUGUI ShakeText;
+    public Text ShakeTextL;
+
+    void Start()
+    {
+        ShakeCount = 0;
+        //ShakeText = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        preAcceleration = Acceleration;
+        Acceleration = Input.acceleration;
+        DotProduct = Vector3.Dot(Acceleration, preAcceleration);
+        if (DotProduct < 0)
+        {
+            ShakeCount++;
+        }
+        Debug.Log("ShakeCount = " + ShakeCount);
+        //ShakeText.text = ShakeCount.ToString("000") + "‰ñ";
+        ShakeTextL.text = ShakeCount.ToString("000") + "‰ñ";
+        //AppleText.text = Apple.ToString() + "ŒÂŠŽ";
+
+
+        if (ShakeCount == 10)
+        {
+            // SceneManager.LoadScene("Clear");
+            //ShakeText.text = ("Clear");
+            ShakeTextL.text = ("Clear");
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/Mission Scripts/TimeLimit.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimeLimit : MonoBehaviour
+{
+	//public Text timerText;
+	public TextMeshProUGUI TimerText;
+
+	public float totalTime;
+	int seconds;
+	int End = 12345;
+
+	// Use this for initialization
+	void Start()
+	{
+		//TimerText = GetComponent<TextMeshProUGUI>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		totalTime -= Time.deltaTime;
+		seconds = (int)totalTime;
+		TimerText.text = seconds.ToString();
+
+
+		if (totalTime <= 1)
+		{
+			totalTime = 0;
+			Limit();
+			//this.gameObject.SetActive(true);
+		}
+
+	}
+
+	public void Limit()
+	{
+		TimerText.text = End.ToString();
+	}
+
+}
