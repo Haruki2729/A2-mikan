38ca5ee34742a258ea0103a154b9f433d9b6b279
uwabaking <s256128@wakayama-u.ac.jp>
2022-07-20 21:07:14 +0900

    現在時刻とるスクリプト

---
+++ Assets/Scripts/UnityAlarm/GetTime.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GetTime : MonoBehaviour
+{
+
+    public TextMeshProUGUI ClockText;
+    public string HourNowstr;
+    public string MinuteNowstr;
+    public string SecondNowstr;
+    public string TimeNowstr;
+    public static string SetTimestr;
+    public static int SetTimeInt = 0;
+    public static int TimeNowInt;
+    public static int SecondNowInt;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        ClockText = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClockText.text = DateTime.Now.ToLongTimeString();
+        //Debug.Log("åªç›éû = " + DateTime.Now.Hour.ToString());
+
+        /*
+        //SecondNow = DateTime.Now.Second();
+        //Debug.Log("SecondNow = " + SecondNow);
+        // éÊìæÇ∑ÇÈíl: ïb
+        HourNowstr = System.DateTime.Now.Hour.ToString();
+        //Debug.Log("HourNowstr = " + HourNowstr);
+
+        MinuteNowstr = System.DateTime.Now.Minute.ToString();
+        MinuteNowstr = MinuteNowstr.PadLeft(2, '0');
+        //Debug.Log("MinuteNowstr = " + MinuteNowstr);
+
+        SecondNowstr = System.DateTime.Now.Second.ToString();
+        SecondNowstr = SecondNowstr.PadLeft(2, '0');
+        //Debug.Log("SecondNowstr = " + SecondNowstr);
+
+
+
+        TimeNowstr = HourNowstr + MinuteNowstr + SecondNowstr;
+        Debug.Log("TimeNowstr = " + TimeNowstr);
+
+        TimeNowInt = Int32.Parse(TimeNowstr);
+        SecondNowInt = Int32.Parse(SecondNowstr);
+
+        if (SecondNowInt == 0)
+        {
+            ArarmCheck(TimeNowInt);
+        }
+        */
+
+    }
+
+    /*
+    public static int getTimeNow()
+    {
+        return TimeNowInt;
+    }
+
+    public static int getTimeSecondNow()
+    {
+        return System.DateTime.Now.Second;
+    }
+
+    public void ArarmCheck(int TimeNowInt)
+    {
+        int SetTimeInt = TimeManager.getSetTime();
+
+        if (SetTimeInt <= TimeNowInt)
+        {
+            Debug.Log("ê›íËéûä‘Ç…Ç»Ç¡ÇΩÇÊ");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+    */
+
+}
