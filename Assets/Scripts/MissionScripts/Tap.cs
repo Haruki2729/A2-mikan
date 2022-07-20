1078a36b3868194dc6e286c5d38436c39fc2994f
uwabaking <s256128@wakayama-u.ac.jp>
2022-07-20 20:12:45 +0900

    Mission2（タップ100回）実装

---
+++ Assets/Scripts/MissionScripts/Tap.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Tap : MonoBehaviour
+{
+
+    public static int TapCount = 0;
+    //public TextMeshProUGUI ShakeText;
+    public Text TapTextL;
+
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClickTapButton()
+    {
+        TapCount++;
+        //TapTextL.text = TapCount.ToString();
+        TapTextL.text = TapCount.ToString("000") + "回";
+        Debug.Log(TapCount);
+
+        
+        if (TapCount == 10)
+        {
+            TapCount = 0;
+            TapTextL.text = ("Clear");
+            Debug.Log("Clear");
+            SceneManager.LoadScene("MissionClearScene");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/MissionScripts/TimeLimit2.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TimeLimit2 : MonoBehaviour
+{
+	//public Text timerText;
+	public TextMeshProUGUI TimerText;
+
+	public float totalTime;
+	int seconds;
+	int End = 0;
+
+	// Use this for initialization
+	void Start()
+	{
+		//TimerText = GetComponent<TextMeshProUGUI>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		totalTime -= Time.deltaTime;
+		seconds = (int)totalTime;
+		TimerText.text = seconds.ToString();
+
+
+		if (totalTime < 1)
+		{
+			totalTime = 0;
+			Limit();
+			//this.gameObject.SetActive(true);
+			SceneManager.LoadScene("MissionFailureScene");
+
+		}
+
+	}
+
+	public void Limit()
+	{
+		TimerText.text = End.ToString();
+	}
+
+}
