18ae7c866545e792a4a30bc3431b5ab933a43b77
DESKTOP-M5G6RRI\NEC-PCuser <s256084@wakayama-u.ac.jp>
2022-08-14 00:00:41 +0900

    コイン無限獲得バグの修正を行いました

--- Assets/Scripts/DateScript.cs
+++ Assets/Scripts/DateScript.cs
@@ -10,6 +10,10 @@ public class DateScript : MonoBehaviour
     public static int Foodgauge = 1;//餌ゲージ初期化
     public static int Date = 1;//日数初期化
 
+    public static int todaycoin = 0;//コインを1日1回しか獲得できないようにするための変数
+
+    bool isCalledOnce = false;
+
     //このスクリプト内だけで使われる変数
     //public static int NowHourTime;
     //public static int NowMinuteTime;
@@ -39,10 +43,16 @@ public class DateScript : MonoBehaviour
         }
 
         //一日経ったら日付を進める
-        if(NowHourTime == 0 && NowMinuteTime == 0 && NowSecondTime == 0)
+        //コイン獲得もリセット
+        if(NowHourTime == 0 && NowMinuteTime == 0 && NowSecondTime == 0 && NowMilliSecondTime == 0)
         {
+            todaycoin = 0;
+
             Date++;
+          
         }
+
+
 
         //if(System.DateTime.Now.Hour==0 && )
 
